d479c97f0db2df3acc15c3d500a7c065d97c6262
johnavacado <devnull@localhost>
2013-03-10 01:07:22 -0600

    added symmetry control function stuff

---
+++ src/org/fleen/samples/fleenRasterCompositionGen/symmetryControlFunction/SCF_Default.java
@@ -0,0 +1,5 @@
+package org.fleen.samples.fleenRasterCompositionGen.symmetryControlFunction;
+
+public class SCF_Default extends SymmetryControlFunction_Abstract{
+
+}
---
+++ src/org/fleen/samples/fleenRasterCompositionGen/symmetryControlFunction/SymmetryControlFunction_Abstract.java
@@ -0,0 +1,8 @@
+package org.fleen.samples.fleenRasterCompositionGen.symmetryControlFunction;
+
+public abstract class SymmetryControlFunction_Abstract{
+  
+  public String toString(){
+    return getClass().getSimpleName();}
+
+}
